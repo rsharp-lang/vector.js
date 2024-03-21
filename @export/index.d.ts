53c93e312444ce1d81c0fa723963d7083f5c50b8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-21 18:50:33 +0800

    ignore modules

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=vector.js
+
+declare namespace vector.js {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
