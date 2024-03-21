a3de72f894df7612dfb55730cd4effc76edff850
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-21 15:40:33 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 
-
 const .onLoad = function() {
-    
+    # register the javascript language interpreter
+    # and language runtime into R# environment
+    # on package load
+    .Internal::register(["*.js", "*.ts"], assembly("vjs.dll"));
 }
